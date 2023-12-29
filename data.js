1b035dbcc182c05bf30c8da94943ad32f175adc5
nikonursaputra96 <nikonursaputra96@gmail.com>
2023-12-29 16:24:55 +0700

    edited some javascript variable

--- data.js
+++ data.js
@@ -8,9 +8,8 @@ function sendEmail() {
     let textarea = document.getElementById('textarea').value
 
 
-   let send = "mailto:nikonursaputra@gmail.com" + "?subject=" + " Halo my name is " + encodeURIComponent(name) + " let's Talk " + "&body=" + "\nLet me introduce myself: " + "I'm " + encodeURIComponent(name) + " \n My email  " + encodeURIComponent(email) + " \n My phone number " + encodeURIComponent(tel) + "\nWe are  " + encodeURIComponent(option)+ " account " + " \n I think you're  "+ encodeURIComponent(textarea);
+   let send = "mailto:nikonursaputra@gmail.com" + "?subject=" + " Halo my name is " + encodeURIComponent(name) + " let's Talk " + "&body=" + "Let me introduce myself: " + "I'm " + encodeURIComponent(name) + "  My email  " + encodeURIComponent(email) + "  My phone number " + encodeURIComponent(tel) + "We are  " + encodeURIComponent(option)+ " account " + "  I think you're  "+ encodeURIComponent(textarea);
 
 
-    console.log (link)
     window.location.href = send
 }
