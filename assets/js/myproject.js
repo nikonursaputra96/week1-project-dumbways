f27faf4f4e1dd9326065487c0f6896a6a6e373be
nikonursaputra96 <nikonursaputra96@gmail.com>
2024-01-04 20:36:54 +0700

    Add date time function, count the days duration from start and end date

--- assets/js/myproject.js
+++ assets/js/myproject.js
@@ -1,8 +1,8 @@
 const myProjects = [];
 
-function myProject (e) {
+function myProject(e) {
     e.preventDefault()
-    
+
     const projectName = document.getElementById('projectName').value;
     let startDate = new Date(document.getElementById('startDate').value);
     let endDate = new Date(document.getElementById('endDate').value);
@@ -12,13 +12,29 @@ function myProject (e) {
     let nextjs = document.getElementById('nextjs').checked;
     let typescript = document.getElementById('typescript').checked;
     let image = document.getElementById('image').files;
-    image= URL.createObjectURL(image[0]);
-    
+    image = URL.createObjectURL(image[0]);
+
+
 
     // Calculation duration
-    let minDate = endDate - startDate;
-    let minMonth = minDate / (1000 * 60 * 60 * 24 * 30);
-    let totalMonth = Math.floor(minMonth)
+
+    function durationMonthDate(startDate, endDate) {
+
+        let date = endDate - startDate;
+        let month = date / (1000 * 60 * 60 * 24 * 30);
+        let totalMonth = Math.floor(month)
+
+        if (totalMonth === 1) {
+            return totalMonth + ' bulan'
+        } else if (totalMonth < 1) {
+            let day = date / (1000 * 60 * 60 * 24)
+            return Math.floor(day) + ' hari'
+        } else {
+            return totalMonth + ' bulan'
+        }
+    }
+
+    const totalMonth = durationMonthDate(startDate, endDate);
 
     const myProject = {
         projectName,
@@ -34,7 +50,7 @@ function myProject (e) {
     }
 
     myProjects.unshift(myProject)
-    renderMyProject ()
+    renderMyProject()
 
 
 }
@@ -42,30 +58,30 @@ function myProject (e) {
 // Delete 1 card
 function deleteProject(index) {
     myProjects.splice(index, 1);
-    renderMyProject(); 
+    renderMyProject();
 }
 
 
-function renderMyProject () {
+function renderMyProject() {
 
     let html = ''
 
- 
-    for(let i = 0 ; i < myProjects.length ; i++) {
-       
+
+    for (let i = 0; i < myProjects.length; i++) {
+   
         html += `<div class="container-card">
         <div class="container-card-2">
             <div class="container-content">
                 <img src="${myProjects[i].image}" alt="smartphone" class="image-1">
                 <h4>${myProjects[i].projectName}</h4>
-                <span  id="totalMonth">${myProjects[i].totalMonth} bulan</span>
+                <span  id="totalMonth">${myProjects[i].totalMonth}</span>
                 <p>${myProjects[i].description}</p>
             </div>
             <div class="container-logo">
                 <img src="${myProjects[i].nodejs ? 'assets/img/nodejs.png' : ''}" alt="logo-nodejs" class="logo-nodejs" id="logoNodejs" style="${!myProjects[i].nodejs ? 'display: none;' : ''}">
-                <img src="${myProjects[i].reactjs ? "assets/img/reactjs.png" : "" }" alt="logo-reactjs" class="logo-reactjs"  id="logoReactjs" style="${!myProjects[i].reactjs ? 'display: none;' : ''}">
-                <img src="${myProjects[i].nextjs ? "assets/img/nextjs.png" : "" }"  alt="logo-nextjs" class="logo-nextjs" id="logoNextjs" style="${!myProjects[i].nextjs ? 'display: none;' : ''}">
-                <img src="${myProjects[i].typescript ? "assets/img/typescript.png" : "" }"  alt="logo-typescript" class="logo-typescript" id="logoTypescript" style="${!myProjects[i].typescript ? 'display: none;' : ''}">
+                <img src="${myProjects[i].reactjs ? "assets/img/reactjs.png" : ""}" alt="logo-reactjs" class="logo-reactjs"  id="logoReactjs" style="${!myProjects[i].reactjs ? 'display: none;' : ''}">
+                <img src="${myProjects[i].nextjs ? "assets/img/nextjs.png" : ""}"  alt="logo-nextjs" class="logo-nextjs" id="logoNextjs" style="${!myProjects[i].nextjs ? 'display: none;' : ''}">
+                <img src="${myProjects[i].typescript ? "assets/img/typescript.png" : ""}"  alt="logo-typescript" class="logo-typescript" id="logoTypescript" style="${!myProjects[i].typescript ? 'display: none;' : ''}">
             </div>
             <div class="container-button">
                 <button onclick="window.location.href='detail.html?id=${i}'" class="edit-button">edit</button>
@@ -74,7 +90,12 @@ function renderMyProject () {
         </div>
       
     </div>`
+
+    
     }
+
+    
+
 
     document.getElementById('container').innerHTML = html
 
@@ -86,4 +107,4 @@ function renderMyProject () {
 
 }
 
-renderMyProject ()
+renderMyProject()
